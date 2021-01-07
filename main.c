3c6095b09633a5e128030d779bc40a4e567c9ceb
Yunus Emre <yunusemre@localhost.localdomain>
2021-01-07 12:24:32 +0300

    Argument error checking is handled

--- main.c
+++ main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+
+int checkDigit(char temp[]){
+	int length = strlen(temp);
+	int i = 0;
+
+	for(i = 0 ; i < length; i++){
+		if(isdigit(temp[i])){
+			return 1;
+		}
+	}
+
+	return 0;
+}
 
 int main(int argc, char *argv[]){
 
 	//argc -> girilen argüman sayısını veriyor
-
-	printf("total arguman sayısı : %d\n",argc);
+	if(argc != 10){
+		printf("Please check your arguments!\nPlease just write 0 , If you do not want to enter any threads!\n");
+		return -1;
+	}
 
 	int i = 0;
 	int numPublisherType = -1;
@@ -25,19 +41,44 @@ int main(int argc, char *argv[]){
 		else if(strcmp(argv[i],"-b") == 0){sc = 1;} // if option is -b
 		else if(strcmp(argv[i],"-s") == 0){sc = 2;} // // if option is -s
 
+
 		switch(sc){
-			case 0:
+			case 0:{ // -n
+			char temp1[10]; char temp2[10]; char temp3[10];
+			strcpy(temp1,argv[i+1]); strcpy(temp2,argv[i+2]); strcpy(temp3,argv[i+3]); //This part indicates that
+			if(!(checkDigit(temp1) && checkDigit(temp2) && checkDigit(temp3))){		   //you have to give 3 digit after -n option.
+				printf("Please check your arguments !\n");
+				return -1;
+			}
 			numPublisherType = atoi(argv[i+1]); // this is for publisher type threads
 			numPublisherCount = atoi(argv[i+2]); // this is for publisher type threads count
 			numPackagerCount = atoi(argv[i+3]); // this is for packager threads count
+
 				break;
-			case 1:
+
+			}
+
+			case 1:{ // -b
+			char temp1[10]; strcpy(temp1,argv[i+1]);
+			if(!(checkDigit(temp1))){						//This part indicates that you have to give 1 digit after -b option
+				printf("Please check your arguments !\n");
+				return -1;
+			}	
 			numPublishingBook = atoi(argv[i+1]); // this indicates how many book each publisher thread can publish
 				break;
-			case 2:
+				}
+				
+			case 2:{ // -s
+			char temp1[10]; char temp2[10];	
+			strcpy(temp1,argv[i+1]); strcpy(temp2,argv[i+2]);
+			if(!(checkDigit(temp1) && checkDigit(temp2))){
+				printf("Please check your arguments !\n");
+				return -1;
+			}
 			numPackagerBook = atoi(argv[i+1]); // this indicates how many book each packager thread can package
 			bufferSize = atoi(argv[i+2]); // this indicates buffer size 
 				break;
+			}
 			default:
 				break;
 		}
